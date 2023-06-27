8a607afd28ec1d34986b1bdc3c5aa700dccc58e7
cotsom <gerfy.17@mail.ru>
2023-06-27 17:28:37 +0300

    upd pki

--- pki/deploy.sh
+++ pki/deploy.sh
@@ -1,11 +1,13 @@
 # docker-compose up -d
 
 docker run -d -v step:/home/step \
-    -p 9000:9000 \
-    -e "DOCKER_STEPCA_INIT_NAME=Smallstep" \
-    -e "DOCKER_STEPCA_INIT_DNS_NAMES=localhost,$(hostname -f)" \
-    -e "DOCKER_STEPCA_INIT_PASSWORD=qwerty"\
-    smallstep/step-ca
+   -p 9000:9000 \
+   -e "DOCKER_STEPCA_INIT_NAME=Smallstep" \
+   -e "DOCKER_STEPCA_INIT_DNS_NAMES=localhost,$(hostname -f)" \
+   -e "DOCKER_STEPCA_INIT_PASSWORD=qwerty"\
+   smallstep/step-ca
+
+sleep 2
 
 CA_FINGERPRINT=$(docker run -v step:/home/step smallstep/step-ca step certificate fingerprint certs/root_ca.crt)
 step ca bootstrap --ca-url https://localhost:9000 --fingerprint $CA_FINGERPRINT
